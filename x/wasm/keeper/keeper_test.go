df725cbf325bb99ae68a819a30ff0c50283947db
slandymani <slandyman9@gmail.com>
2024-11-22 18:32:27 +0200

    fix paths

--- x/wasm/keeper/keeper_test.go
+++ x/wasm/keeper/keeper_test.go
@@ -422,7 +422,7 @@ func TestInstantiate(t *testing.T) {
 
 	gasAfter := ctx.GasMeter().GasConsumed()
 	if types.EnableGasVerification {
-		require.Equal(t, uint64(0x1bc8f), gasAfter-gasBefore)
+		require.Equal(t, uint64(0x1bca5), gasAfter-gasBefore)
 	}
 
 	// ensure it is stored properly
